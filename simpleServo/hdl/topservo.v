5b877635c6785c6e008af4836918a6c5d0f5dbef
Fabien Marteau <fabien.marteau@armadeus.com>
2020-04-09 21:45:50 +0200

    simpleServo: fix syntax

--- simpleServo/hdl/topservo.v
+++ simpleServo/hdl/topservo.v
@@ -3,16 +3,15 @@ module topServo (
     output reg srv_o
 );
 
-
 reg reset;
-rst_gen rst_inst (.clk_i(clk_i), .rst_i(1'b0), .rst_o(reset))
+rst_gen rst_inst (.clk_i(clk_i), .rst_i(1'b0), .rst_o(reset));
 
 SimpleServo sso (
     .clk_i(clk_i),
     .rst_i(reset),
     .en_i(1'b1),
     .position_i(8'h80),
-    .srv_o(srv_o))
+    .srv_o(srv_o));
 
 
 endmodule
