4a17f1b41e0cee4e63fc72bc934f071400666023
Fabien Marteau <fabien.marteau@armadeus.com>
2020-06-23 20:23:02 +0200

    simpleVga: adding hvsync

---
+++ simpleVga/hdl/hvsync.v
@@ -0,0 +1,18 @@
+`timescale 1p/1ps
+/* Horizontal, vertical VGA signals generation
+* for 640 x 480 VGA
+* */
+
+module HVSync(
+    input clk_i,
+    input rst_i,
+    output hsync_o,
+    output vsync_o,
+    output display_on,
+    output hpos[9:0],
+    output vpos[8:0]);
+
+
+
+
+endmodule
